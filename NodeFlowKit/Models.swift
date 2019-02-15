6cdd21f1cc058ccf70900b658975e148fd11f012
Vasilis Akoinoglou <alladinian@gmail.com>
2019-02-15 17:17:21 +0200

    Moved OptionSet extension to Extensions file

--- NodeFlowKit/Models.swift
+++ NodeFlowKit/Models.swift
@@ -23,24 +23,6 @@ import Cocoa
  - A specially formatted image or array of six images, specifying the faces of a cube map
 */
 
-public extension OptionSet where RawValue: FixedWidthInteger {
-    func elements() -> AnySequence<Self> {
-        var remainingBits = rawValue
-        var bitMask: RawValue = 1
-        return AnySequence {
-            return AnyIterator {
-                while remainingBits != 0 {
-                    defer { bitMask = bitMask &* 2 }
-                    if remainingBits & bitMask != 0 {
-                        remainingBits = remainingBits & ~bitMask
-                        return Self(rawValue: bitMask)
-                    }
-                }
-                return nil
-            }
-        }
-    }
-}
 
 public struct ContentType: OptionSet {
     public let rawValue: Int
